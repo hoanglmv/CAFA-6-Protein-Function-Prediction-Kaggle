7bfdafe059aec18e69cc8f28216a15882a8e141c
HoangTung-Vu <minhhoangtungvu04@gmail.com>
2025-12-09 21:59:52 +0700

    new model replace cossim and scale by dotprod

--- src/align_embed/protein_go_aligner.py
+++ src/align_embed/protein_go_aligner.py
@@ -3,38 +3,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class ProteinGOAligner(nn.Module):
-    def __init__(self, esm_dim=2560, go_emb_dim=768, joint_dim=512):
-        super().__init__()
 
-        # --- NHÁNH 1: PROTEIN (Sửa: Bỏ Dropout, giữ BatchNorm) ---
+class ProteinGOAligner(nn.Module):
+    def __init__(self, esm_dim=2560, go_emb_dim=768):
+        super().__init__()
         self.prot_projector = nn.Sequential(
-            nn.Linear(esm_dim, 1024),
+            nn.Linear(esm_dim, 2048),  # Giữ chiều lớn để không mất tin
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Linear(2048, 1024),
             nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Linear(1024, joint_dim)
+            nn.Linear(1024, go_emb_dim),  # Output ra đúng 768 (bằng dim của GO)
         )
-
-        # --- NHÁNH 2: GO LABEL  ---
-        self.go_projector = nn.Sequential(
-            nn.Linear(go_emb_dim, 1024), 
-            nn.BatchNorm1d(1024),        
-            nn.ReLU(),
-            nn.Linear(1024, joint_dim)
-        )
-
-        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.05))
 
     def forward(self, esm_embeddings, go_embeddings):
-        prot_vec = self.prot_projector(esm_embeddings)
-        go_vec = self.go_projector(go_embeddings)
+        """
+        esm_embeddings: [Batch_Size, 2560]
+        go_embeddings:  [Num_GO_Terms, 768] (hoặc Batch_GO, 768)
+        """
 
-        prot_vec = F.normalize(prot_vec, p=2, dim=1)
-        go_vec = F.normalize(go_vec, p=2, dim=1)
+        # 1. Project Protein về không gian 768
+        prot_vec = self.prot_projector(esm_embeddings)  # -> [Batch, 768]
 
-        cosine_sim = torch.matmul(prot_vec, go_vec.T)
+        # 2. GO Vector giữ nguyên (Chỉ chuẩn hóa)
+        go_vec = go_embeddings  # -> [Num_Labels, 768]
 
-        logit_scale = self.logit_scale.exp().clamp(max=100)
-        logits = cosine_sim * logit_scale
+        # 3. Normalize (Bắt buộc cho Cosine)
+        # prot_vec = F.normalize(prot_vec, p=2, dim=1)
+        # go_vec = F.normalize(go_vec, p=2, dim=1)
+
+        # 4. Tính Dot product
+        logits = torch.matmul(prot_vec, go_vec.T)
 
         return logits
